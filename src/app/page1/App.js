acc08f9bb750018b570d671c28f8cd78868c9bfb
Ryosuke Okuda <okuda@kic.ac.jp>
2024-09-03 14:25:26 +0900

    add style

--- src/app/page1/App.js
+++ src/app/page1/App.js
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
+import '../maelie.css';
 
 function App() {
     const [ name, setName ]                 = useState( '' );
@@ -25,28 +26,31 @@ function App() {
     }
       
     return (
-        <>
-            <div>
-                Name: <input type="text" name="name" onChange={handleNameChange} value={name}/>
+        <div className="container">
+            <div className="form-group">
+                <label htmlFor="name">Name:</label><br/>
+                <input type="text" name="name" onChange={handleNameChange} value={name}/>
             </div>
-            <div>
-                Room: <select name="room" onChange={handleRoomChange} value={room}>
-                        <option value="RoomA">RoomA</option>
-                        <option value="RoomB">RoomB</option>
-                        <option value="RoomC">RoomC</option>
-                    </select>                
+            <div className="form-group">
+                <label htmlFor="room_name">Room:</label>
+                <select name="room" onChange={handleRoomChange} value={room}>
+                    <option value="RoomA">RoomA</option>
+                    <option value="RoomB">RoomB</option>
+                    <option value="RoomC">RoomC</option>
+                </select>                
             </div>
-            <div>
-                Date: <input type="date" name="date" onChange={handleDateChange} value={date}/>
+            <div className="form-group">
+                <label htmlFor="date">Date:</label>
+                <input type="date" name="date" onChange={handleDateChange} value={date}/>
             </div>
-            <div>
-                Description: <br/>
+            <div className="form-group">
+                <label htmlFor="description">Description:</label>
                 <textarea rows="8" cols="40" onChange={handleDescriptionChange} value={description}/>
             </div>
-            <div>
+            <div className="form-group">
                 <button> Submit </button>
             </div>
-        </>
+        </div>
     );
 }
 
